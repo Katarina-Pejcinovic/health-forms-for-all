75d637fc056ed19994399afca0ede841b5a28b1a
Nick Pavlosky <44681231+NeekTheGiraffe@users.noreply.github.com>
2023-01-28 12:54:59 -0800

    Add another annotation to script example

--- examples/pypdf-example/script.py
+++ examples/pypdf-example/script.py
@@ -14,7 +14,12 @@ annotation = AnnotationBuilder.text(
     (100, 200, 300, 400),
     "Hello world\nthis is the second line"
 )
+annotation2 = AnnotationBuilder.rectangle(
+    rect=(50, 550, 200, 650),
+    interiour_color='#f52891'
+)
 writer.add_annotation(page_number=0, annotation=annotation)
+writer.add_annotation(page_number=0, annotation=annotation2)
 
 with open('output.pdf', 'wb') as outfile:
     writer.write(outfile)
